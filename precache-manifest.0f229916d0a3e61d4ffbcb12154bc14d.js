cf8d69af80dedc544ada7932c75cca9271855b45
Borsuk <borsuk@macbook-pro-macbook.home>
2020-01-21 23:04:56 +0100

    Updates

---
+++ precache-manifest.0f229916d0a3e61d4ffbcb12154bc14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea00cafc080cbbf179c2a488feb1241",
+    "url": "/lets-pack-app/index.html"
+  },
+  {
+    "revision": "01afdfdc35303eba7b9c",
+    "url": "/lets-pack-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c1f60c599e5093b02d2",
+    "url": "/lets-pack-app/static/js/2.56d464eb.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/lets-pack-app/static/js/2.56d464eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01afdfdc35303eba7b9c",
+    "url": "/lets-pack-app/static/js/main.0c0fedc2.chunk.js"
+  },
+  {
+    "revision": "33cfcea759f0b09c28e6",
+    "url": "/lets-pack-app/static/js/runtime-main.1542fe63.js"
+  }
+]);
